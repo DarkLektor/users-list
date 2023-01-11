fdf0175b29fa69a755c5d9ec826cfc2147d4a885
Kamil <darklektor1@gmail.com>
2023-01-11 18:03:28 +0500

    Deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: 'https://darklektor.github.io/',
+    base: 'https://darklektor.github.io/users-list/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
